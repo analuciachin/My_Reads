fc3d51c506d7d48d9ebcd24df9aae2741769d715
anachin <ananime@gmail.com>
2018-06-04 22:29:52 -0400

    initial files + building main page with 3 shelves

---
+++ src/App.js
@@ -0,0 +1,103 @@
+import React from 'react'
+// import * as BooksAPI from './BooksAPI'
+import './App.css'
+import ListBooks from './ListBooks'
+/*
+const books = [
+  {
+    "id": "1",
+    "coverURL": "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
+    "title": "To Kill a Mockingbird",
+    "authors": "Harper Lee",
+    "shelf": "Currently Reading"
+  },
+  {
+    "id": "2",
+    "coverURL": "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api", 
+    "title": "Ender's Game",
+    "authors": "Orson Scott Card",
+    "shelf": "Currently Reading" 
+  }
+]
+*/
+
+class BooksApp extends React.Component {
+  state = {
+    reading: [
+      {
+        "id": "0",
+        "coverURL": "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
+        "title": "To Kill a Mockingbird",
+        "authors": "Harper Lee",
+        "shelf": "Currently Reading"
+      },
+      {
+        "id": "1",
+        "coverURL": "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api", 
+        "title": "Ender's Game",
+        "authors": "Orson Scott Card",
+        "shelf": "Currently Reading"
+      }
+    ],
+    wantToRead: [
+      {
+        "id": "3",
+        "coverURL": "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api",
+        "title": "1776",
+        "authors": "David McCullough",
+        "shelf": "Want to Read"
+      },
+      {
+        "id": "4",
+        "coverURL": "http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api",
+        "title": "Harry Potter and the Sorcerer's Stone",
+        "authors": "J.K. Rowling",
+        "shelf": "Want to Read"
+      }
+    ],
+    read: [
+      {
+        "id": "5",
+        "coverURL": "http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api",
+        "title": "The Hobbit",
+        "authors": "J.R.R. Tolkien",
+        "shelf": "Read" 
+      },
+      {
+        "id": "6",
+        "coverURL": "http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api", 
+        "title": "Oh, the Places You'll Go!",
+        "authors": "Seuss",
+        "shelf": "Read" 
+      }
+    ]
+    
+    /**
+     * TODO: Instead of using this state variable to keep track of which page
+     * we're on, use the URL in the browser's address bar. This will ensure that
+     * users can use the browser's back and forward buttons to navigate between
+     * pages, as well as provide a good URL they can bookmark and share.
+     */
+    //showSearchPage: false
+  }
+
+
+  removeReading = (book, from, to) => {
+    console.log(book);  
+    this.setState((state) => ({
+      reading: state.reading.filter(b => book.id !== b.id )
+    }))
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <ListBooks onDeleteReading={this.removeReading} books={this.state.reading} currentShelf="reading" />
+        <ListBooks books={this.state.wantToRead} />
+        <ListBooks books={this.state.read} />
+      </div>
+    )
+  }
+}
+
+export default BooksApp
---
+++ src/ListBooks.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+//import PropTypes from 'prop-types'
+
+class ListBooks extends Component {
+ 
+	render() {
+		return (
+			<div className="app">
+			<div className="list-books">
+            	<div className="list-books-title">
+              		<h1>My Reads</h1>
+            	</div>
+				<div className="list-books-content">
+					<div className="bookshelf">
+                  		
+					{/*<h2 className="bookshelf-title">{this.props.books[0].shelf}</h2>*/}
+						<div className="bookshelf-books">
+							<ol className='books-grid'>
+								<li>
+								{this.props.books.map((book) => (
+									<div key={book.id} className='book'>
+										<div className='book-top'>
+											<div className='book-cover' style={{
+												backgroundImage: `url(${book.coverURL})`, width: 128, height: 193}}>
+											</div> {/*div book-cover*/}
+											<div className='book-shelf-changer'>
+                              					<select id='selectedShelf' onChange={() => this.props.onDeleteReading(book)}>
+		                                			<option value='none' disabled>Move to...</option>
+		                                			<option value='currentlyReading'>Currently Reading</option>
+		                                			<option value='wantToRead'>Want to Read</option>
+		                                			<option value='read'>Read</option>
+		                                			<option value='none'>None</option>
+		                            			</select>
+                            				</div> {/*div book-shelf-changer*/}
+										</div> {/*book-top*/}
+										<div className='book-title'>{book.title}</div>
+                        				<div className='book-authors'>{book.authors}</div>
+                        				<div className='book-authors'>{book.shelf}</div>
+									</div> //div book	
+								))}
+								</li>
+							</ol>
+						</div> {/*div bookshelf-books*/}
+					</div> {/*div bookshelf*/}
+				</div> {/*div list-books-content*/}
+			</div> {/*div list-books*/}
+			</div> /*div app*/
+		)
+	}
+
+}
+
+export default ListBooks
+
