d78c7c94f17a690c9768d3696f12d7e4750164ff
anachin <ananime@gmail.com>
2018-08-02 23:01:50 -0400

    deleted unused file and converted ListBooks from class to function

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,45 +1,42 @@
-import React, { Component } from 'react'
-//import { Link } from 'react-router-dom'
 
-class ListBooks extends Component {
+function ListBooks(props) {
  
-	render() {
-		return (
-			<div className="list-books-content">
-				<div className="bookshelf">
-				<h2 className="bookshelf-title">{this.props.currentShelfTitle}</h2>
-					<div className="bookshelf-books">
-						<ol className='books-grid'>
-							<li>
-							{this.props.books.map((book) => (
-								<div key={book.id} className='book'>
-									<div className='book-top'>
-										<div className='book-cover' style={{
-											backgroundImage: `url(${book.imageLinks.smallThumbnail})`, width: 128, height: 193}}>
-										</div> {/*div book-cover*/}
-										<div className='book-shelf-changer'>
-	                      					<select id={'book' + book.id} onClick={() => this.props.onDisableShelfOption(book)} onChange={() => this.props.onChangeShelves(book)}>
-	                                			<option value='none'>Move to...</option>		                                			
-	                                			<option value='currentlyReading' disabled={book.shelf === 'currentlyReading'}>Currently Reading</option>
-	                                			<option value='wantToRead' disabled={book.shelf === 'wantToRead'}>Want to Read</option>
-	                                			<option value='read' disabled={book.shelf === 'read'}>Read</option>
-	                                			<option value='none' disabled={book.shelf === 'none'}>None</option>
-	                            			</select>
-	                    				</div> {/*div book-shelf-changer*/}
-									</div> {/*book-top*/}
-									<div className='book-title'>{book.title}</div>
-	                				<div className='book-authors'>{book.authors}</div>
-								</div> //div book	
-							))}
-							</li>
-						</ol>
-					</div> {/*div bookshelf-books*/}
-				</div> {/*div bookshelf*/}
-			</div> /*div list-books-content*/
-		)
-	}
-
+	return (
+		<div className="list-books-content">
+			<div className="bookshelf">
+			<h2 className="bookshelf-title">{props.currentShelfTitle}</h2>
+				<div className="bookshelf-books">
+					<ol className='books-grid'>
+						<li>
+						{props.books.map((book) => (
+							<div key={book.id} className='book'>
+								<div className='book-top'>
+									<div className='book-cover' style={{
+										backgroundImage: `url(${book.imageLinks.smallThumbnail})`, width: 128, height: 193}}>
+									</div> {/*div book-cover*/}
+									<div className='book-shelf-changer'>
+                      					<select id={'book' + book.id} onClick={() => props.onDisableShelfOption(book)} onChange={() => props.onChangeShelves(book)}>
+                                			<option value='none'>Move to...</option>		                                			
+                                			<option value='currentlyReading' disabled={book.shelf === 'currentlyReading'}>Currently Reading</option>
+                                			<option value='wantToRead' disabled={book.shelf === 'wantToRead'}>Want to Read</option>
+                                			<option value='read' disabled={book.shelf === 'read'}>Read</option>
+                                			<option value='none' disabled={book.shelf === 'none'}>None</option>
+                            			</select>
+                    				</div> {/*div book-shelf-changer*/}
+								</div> {/*book-top*/}
+								<div className='book-title'>{book.title}</div>
+                				<div className='book-authors'>{book.authors}</div>
+							</div> //div book	
+						))}
+						</li>
+					</ol>
+				</div> {/*div bookshelf-books*/}
+			</div> {/*div bookshelf*/}
+		</div> /*div list-books-content*/
+	)
 }
+
+
 
 export default ListBooks
 
